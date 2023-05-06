08efaf9131a620e86d75990012b116c836c03a38
Mamata <100906354+mamata171@users.noreply.github.com>
2023-05-06 11:52:55 +0530

    commit

--- static/script.js
+++ static/script.js
@@ -2,20 +2,20 @@ console.log("hello world")
 
 
 
-function musicfun(){
-  element = document.getElementById("photos")
-  if (element.style.display === "" || element.style.display === "none") {
-    element.style.display = "block";
-  }
-}
+// function musicfun(){
+//   element = document.getElementById("photos")
+//   if (element.style.display === "" || element.style.display === "none") {
+//     element.style.display = "block";
+//   }
+// }
 
-function musichide() {
-  element = document.getElementById("photos")
-  if (element.style.display === "block") {
-    element.style.display = "none";
-  } 
+// function musichide() {
+//   element = document.getElementById("photos")
+//   if (element.style.display === "block") {
+//     element.style.display = "none";
+//   } 
 
-}
+// }
 
 
 
